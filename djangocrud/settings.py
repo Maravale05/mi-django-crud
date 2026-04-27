2a9d5cea6b36ca380980b4c4a3b0004d0f35284c
Maravale05 <cedenochumomara@gmail.com>
2026-04-26 22:43:18 -0500

    Fix ALLOWED_HOSTS

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7e%+g7ak4$)^=6wq16wnr(f_xb860su#%#*x#p&+h8t(9-_p5%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mi-django-crud.onrender.com']
 
 
 # Application definition
